acf64612eb68b07a0f35dd1f4acd4c69c8c42eae
Kevin Backhouse <kevinbackhouse@github.com>
2022-02-24 09:30:22 +0000

    DIY version of strerror is no longer needed.

--- src/util/strerror.c
+++ src/util/strerror.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 
+#if 0
 extern int sys_nerr;
 extern char *sys_errlist[];
 extern int errno;
@@ -9,3 +10,4 @@ char *strerror(int num)
     if (num>=0 && num<=sys_nerr) return sys_errlist[num];
     return 0;
 }
+#endif
