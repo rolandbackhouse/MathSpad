b6e0beb5c9f15cb211a7bec71602e6aaf2c6afaf
Roland Backhouse <roland@backhouse.org>
2026-01-02 11:35:08 +0000

    Script for installing required packages in Ubuntu VM.

---
+++ scripts/ubuntu-vm-setup.sh
@@ -0,0 +1,8 @@
+# Utilities for the virtual machine
+apt-get install spice-vdagent spice-webdavd qemu-guest-agent git emacs tmux openssh-server meld libvirt-clients-qemu
+
+# Fonts needed by mathpad
+apt-get install xterm fonts-dejavu-core fonts-dejavu-extra fonts-dejavu-mono xfonts-*
+
+# mathpad build dependencies
+apt-get install byacc build-essential cmake clang dh-autoreconf libx11-dev
